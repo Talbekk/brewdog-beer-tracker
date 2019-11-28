c06af59ec436b0b023ed0ac508c4dbd27cc7a8f5
Talbekk <mt.campbell@outlook.com>
2019-11-28 12:59:10 +0000

    Adds beer information to component

--- src/components/BeerLabel.vue
+++ src/components/BeerLabel.vue
@@ -1,6 +1,11 @@
 <template lang="html">
 <div v-if='beer'>
-  <p>{{beer.name}}</p>
+  <h3>{{beer.name}}</h3>
+  <p>Tagline: {{beer.tagline}}</p>
+  <p>First Brewing: {{beer.first_brewed}}</p>
+  <p>Description: {{beer.description}}</p>
+  <p>ABV: {{beer.abv}}%</p>
+  <img class="label-pic" :src="beer.image_url"/>
 </div>
 </template>
 
