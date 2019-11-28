4217033e6bd46155c4e3e2dcd4e66c17d0ef1765
Talbekk <mt.campbell@outlook.com>
2019-11-28 11:17:35 +0000

    adds beers list component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template lang="html">
 
+<div>
+  <beers-list :beers='beers'/>
+</div>
 </template>
 
 <script>
+
+import BeersList from './components/BeersList.vue';
+
 export default {
+  name:'app',
   data() {
     return {
       beers: []
@@ -13,7 +20,9 @@ export default {
     fetch('https://api.punkapi.com/v2/beers')
     .then(response => response.json())
     .then(beers => this.beers = beers)
-    console.log(this.beers);
+  },
+  components: {
+    "beers-list": BeersList
   }
 }
 </script>
--- src/components/BeersList.vue
+++ src/components/BeersList.vue
@@ -1,9 +1,11 @@
 <template lang="html">
-
+<h1>Beers List:</h1>
 </template>
 
 <script>
 export default {
+  name: 'beers-list',
+  props: ['beers']
 }
 </script>
 
