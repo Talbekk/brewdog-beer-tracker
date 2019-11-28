e68e579468ff9ffdcac52313957e4e369308f476
Talbekk <mt.campbell@outlook.com>
2019-11-28 14:16:34 +0000

    toggle favorites button

--- src/components/BeerComponent.vue
+++ src/components/BeerComponent.vue
@@ -1,9 +1,12 @@
 <template lang="html">
-  <li v-on:click="grabABeer">
+  <li  v-if="this.status || (!this.status && !favoriteFilter)">
     <h3>{{beer.name}}</h3>
     <p>{{beer.abv}}</p>
     <p>{{beer.tagline}}</p>
     <img class="beer-icon" src="../../public/beer_icon.svg"/>
+    <input type="checkbox" id="favorite" v-model="status">
+    <label for="favorite" v-if="!status">Add to favorites</label>
+    <button type="button" v-on:click="grabABeer">View Info</button>
   </li>
 </template>
 
@@ -12,7 +15,12 @@ import {eventBus} from '../main.js'
 
 export default {
   name: 'beer-component',
-  props: ['beer'],
+  props: ['beer', 'favoriteFilter'],
+  data() {
+    return {
+      status: false
+    }
+  },
   methods: {
     grabABeer() {
       eventBus.$emit('beer-in-hand', this.beer);
--- src/components/BeersList.vue
+++ src/components/BeersList.vue
@@ -2,8 +2,9 @@
 
 <div id="listContainer">
   <h1>Beers List:</h1>
+  <button type="button" v-on:click="favoriteFilter = !favoriteFilter">{{ favoriteFilter ? "Show All" : "Show Favorites" }}</button>
   <ul>
-    <beer-component v-for="(beer, index) in beers" :beer='beer' :key='index'></beer-component>
+    <beer-component v-for="(beer, index) in beers" :beer='beer' :favoriteFilter="favoriteFilter" :key='index'></beer-component>
   </ul>
 </div>
 </template>
@@ -14,9 +15,21 @@ import BeerComponent from './BeerComponent.vue';
 export default {
   name: 'beers-list',
   props: ['beers'],
+  data() {
+    return {
+    favoriteFilter: false
+  }
+  },
   components: {
     "beer-component": BeerComponent
   }
+  //   filterByFavorite() {
+  //   if (this.favoriteFilter === true){
+  //   this.beers.filter(beer => beer.status)
+  //   console.log(this.beers);
+  // }
+    // }
+  // }
 }
 
 </script>
