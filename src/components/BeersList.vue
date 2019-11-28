6b57e655885b169412f72ae5b2a54f65f305cc48
Talbekk <mt.campbell@outlook.com>
2019-11-28 11:20:39 +0000

    setup BeerComponent

--- src/components/BeersList.vue
+++ src/components/BeersList.vue
@@ -1,5 +1,10 @@
 <template lang="html">
 <h1>Beers List:</h1>
+<div id="list-container">
+  <ul>
+    <beer-component v-for="(beer, index) in beers" :beer="beer" :key="index"/>
+  </ul>
+</div>
 </template>
 
 <script>
